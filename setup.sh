9c424cbce053968fe41545d2cb1b9648f9cc5f3f
Ravi <57653662+ravi-murugesan@users.noreply.github.com>
2020-03-25 16:27:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit
 
 echo "\
+[general]\n\
+email = \"ravi.murugesan1@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
